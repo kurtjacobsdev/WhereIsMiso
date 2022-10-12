6edb1c4a82dba96cff9ed34a542a33893646b53b
Kurt Jacobs <kurtjacobsdev@gmail.com>
2022-10-12 22:21:02 +0200

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 set -e
 
+which openssl
 brew install openssl
 echo 'export PATH="/usr/local/opt/openssl@3/bin:$PATH"' >> ~/.zshrc
 
@@ -10,6 +11,7 @@ pwd
 ls $CI_WORKSPACE
 ls $CI_WORKSPACE/WhereIsMiso
 openssl version
+which openssl
 
 openssl enc -aes-256-cbc -d -in $CI_WORKSPACE/WhereIsMiso/GoogleService-Info.plist.encrypted -out $CI_WORKSPACE/WhereIsMiso/GoogleService-Info.plist -k $GOOGLE_PLIST_DECRYPT_PASSWORD
 
