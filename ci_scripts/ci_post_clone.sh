d72a27211b9fba46a77997cb394a0dac39c7b246
Kurt Jacobs <kurtjacobsdev@gmail.com>
2022-10-12 22:00:36 +0200

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,6 +4,9 @@ set -e
 echo $CI_PROJECT_FILE_PATH
 echo $CI_WORKSPACE
 pwd
+ls $CI_WORKSPACE
+ls $CI_WORKSPACE/WhereIsMiso
+
 openssl enc -aes-256-cbc -d -in $CI_WORKSPACE/WhereIsMiso/GoogleService-Info.plist.encrypted -out $CI_WORKSPACE/WhereIsMiso/GoogleService-Info.plist -k $GOOGLE_PLIST_DECRYPT_PASSWORD
 
 openssl enc -aes-256-cbc -d -in ../WhereIsMiso/GoogleService-Info.plist.encrypted -out ../WhereIsMiso/GoogleService-Info.plist -k $GOOGLE_PLIST_DECRYPT_PASSWORD
