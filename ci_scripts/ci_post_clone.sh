dd02f567429d6dc8965f8006375e445295069844
Kurt Jacobs <kurtjacobsdev@gmail.com>
2022-10-12 22:26:31 +0200

    fixed post clone decrypt script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,18 +1,9 @@
 #!/bin/zsh
 set -e
 
-which openssl
 brew install openssl
 echo 'export PATH="/usr/local/opt/openssl@3/bin:$PATH"' >> ~/.zshrc
 source ~/.zshrc
-
-echo $CI_PROJECT_FILE_PATH
-echo $CI_WORKSPACE
-pwd
-ls $CI_WORKSPACE
-ls $CI_WORKSPACE/WhereIsMiso
-openssl version
-which openssl
 
 openssl enc -aes-256-cbc -d -in $CI_WORKSPACE/WhereIsMiso/GoogleService-Info.plist.encrypted -out $CI_WORKSPACE/WhereIsMiso/GoogleService-Info.plist -k $GOOGLE_PLIST_DECRYPT_PASSWORD
 
